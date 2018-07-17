d36343a8d789d351d9d13697959688424c8c9b6d
James M <saintjames@MacBook-Pro-2.local>
2018-07-17 09:30:55 -0600

    Commit 3 - queried for vampires

--- vampire_app/app.js
+++ vampire_app/app.js
@@ -35,60 +35,67 @@ db.on('disconnected', () => {
 /////////////////////////////////////////////////
 // INSERT USING MONGOOSE
 // ### Add the vampire data that we gave you
+
 // Vampire.collection.insertMany(VampireData,(err, data) => {
 //     console.log("added provided vampire data")
 //     mongoose.connection.close();
 //   });
 
 // ### Add some new vampire data
-Vampire.create({
-  name: 'Buck',
-  hair_color: 'blonde',
-  eye_color: 'red',
-  dob: new Date(1971, 2, 13, 7, 47),
-  loves: ['trucks','blood'],
-  location: 'Minneapolis, Minnesota, US',
-  gender: 'm',
-  victims: 3
-})
 
-Vampire.create({
-  name: 'Larry',
-  hair_color: 'brown',
-  eye_color: 'red',
-  dob: new Date(1971, 2, 13, 7, 47),
-  loves: ['board-games','murder'],
-  location: 'Minneapolis, Minnesota, US',
-  gender: 'm',
-  victims: 9
-})
+// Vampire.create({
+//   name: 'Buck',
+//   hair_color: 'blonde',
+//   eye_color: 'red',
+//   dob: new Date(1971, 2, 13, 7, 47),
+//   loves: ['trucks','blood'],
+//   location: 'Minneapolis, Minnesota, US',
+//   gender: 'm',
+//   victims: 3
+// })
 
-Vampire.create({
-  name: 'Claudia',
-  hair_color: 'blonde',
-  eye_color: 'red',
-  dob: new Date(1971, 2, 13, 7, 47),
-  loves: ['broadway','sacrifice'],
-  location: 'Minneapolis, Minnesota, US',
-  gender: 'm',
-  victims: 4
-})
+// Vampire.create({
+//   name: 'Larry',
+//   hair_color: 'brown',
+//   eye_color: 'red',
+//   dob: new Date(1971, 2, 13, 7, 47),
+//   loves: ['board-games','murder'],
+//   location: 'Minneapolis, Minnesota, US',
+//   gender: 'm',
+//   victims: 9
+// })
 
-Vampire.create({
-  name: 'May',
-  hair_color: 'brown',
-  eye_color: 'red',
-  dob: new Date(1971, 2, 13, 7, 47),
-  loves: ['acult','dating'],
-  location: 'Minneapolis, Minnesota, US',
-  gender: 'm',
-  victims: 17
-})
+// Vampire.create({
+//   name: 'Claudia',
+//   hair_color: 'blonde',
+//   eye_color: 'red',
+//   dob: new Date(1971, 2, 13, 7, 47),
+//   loves: ['broadway','sacrifice'],
+//   location: 'Minneapolis, Minnesota, US',
+//   gender: 'f',
+//   victims: 4
+// })
+
+// Vampire.create({
+//   name: 'May',
+//   hair_color: 'brown',
+//   eye_color: 'red',
+//   dob: new Date(1971, 2, 13, 7, 47),
+//   loves: ['acult','dating'],
+//   location: 'Minneapolis, Minnesota, US',
+//   gender: 'f',
+//   victims: 17
+// })
 mongoose.connection.close();
 /////////////////////////////////////////////////
 // ## QUERYING
 /////////////////////////////////////////////////
 // ### Select by comparison
+1. db.vampires.find({'gender': 'f'})
+2. db.vampires.find({victims: {$gt: 500}})
+3. db.vampires.find({victims: {$lte: 150}})
+4. db.vampires.find({victims: {$ne: 210234}})
+5. db.vampires.find({victims: {$gt: 150, $lt: 500}})
 
 /////////////////////////////////////////////////
 // ### Select by exists or does not exist
